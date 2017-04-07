9a04f9db81d5e0df67abd44beb685dbba497fd02
Daniel Makover <dmakover@gmail.com>
2017-04-07 17:37:04 +0300

    ADDED: randomized alien movements

--- js/prefabs/alien.js
+++ js/prefabs/alien.js
@@ -13,13 +13,42 @@ Asteroids.Alien = function(game, x, y){
 
 Asteroids.Alien.prototype = Object.create(Phaser.Sprite.prototype);
 Asteroids.Alien.prototype.constructor = Asteroids.Alien;
+Asteroids.Alien.prototype.getRandomY = function(){
+  return Math.random()*100;
+};
+Asteroids.Alien.prototype.getRandomY = function(){
+  var x =  Math.random();
+  if(x >= 0.75){return 1;}
+  else if(x >= 0.25 && x <= 0.75){return 2;}
+  else{return 3;}
+};
+
+Asteroids.Alien.prototype.yHolder = 0;
+Asteroids.Alien.prototype.motionHolder = 0;
 
 Asteroids.Alien.prototype.update = function(){
-  console.log(this.randomY);
-  this.body.velocity.y = 12;
-  this.body.velocity.x = -150;
+  this.yHolder =  this.yHolder + 0.05;
+//  console.log(this.yHolder);
+  if(this.motionHolder == 0){
+    this.body.velocity.y = Math.sin(this.yHolder) * 100;
+  }else{
+    this.body.velocity.y = Math.cos(this.yHolder) * 100;
+  }
+
+  this.body.velocity.x = -120;
   if(this.body.x <= -20){
+    this.yHolder = 0;
     this.body.x = 600;
-    this.body.y = 100;
+    if(this.getRandomY() == 1){
+      this.motionHolder = 0;
+      this.body.y = 50;
+    }else if(this.getRandomY() == 2){
+      this.motionHolder = 1;
+      this.body.y = 150;
+    }else{
+      this.motionHolder = 0;
+      this.body.y = 250;
+    }
+    console.log(this.body.y);
   }
 }
